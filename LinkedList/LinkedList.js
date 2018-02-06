e086563301de5b1ee33588301d7fed4b45c143f5
Evgeny Gudkov <gudkoveyu@gmail.com>
2018-02-06 09:52:23 +0300

    Push, pop are added.

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -45,4 +45,56 @@ class LinkedList {
         }
         iter.value = value;
     }
+
+    push() {
+        let iter = this.head;
+
+        for (let i = 0; i < this.length; i++) {
+            iter = iter.next;
+        }
+
+        for (let i = 0; i < arguments.length; i++) {
+            iter.next = {
+                value: arguments[i],
+                next: null
+            };
+            iter = iter.next;
+            this.length++;
+        }
+    }
+
+    pop() {
+        let iter = this.head;
+
+        for (let i = 0; i < this.length - 1; i++) {
+            iter = iter.next;
+        }
+
+        iter.next = null;
+        this.length--;
+    }
 }
+
+let list = new LinkedList(1, 23, 44, 'dsfs', {});
+
+for (let i = 0; i < list.length; i++) {
+    console.log(list.get(i));
+}
+console.log("\n");
+
+list.push('dsdf', 12);
+
+for (let i = 0; i < list.length; i++) {
+    console.log(list.get(i));
+}
+
+console.log("\n");
+console.log("List length: " + list.length + "\n");
+
+list.pop();
+
+for (let i = 0; i < list.length; i++) {
+    console.log(list.get(i));
+}
+console.log("\n");
+console.log("List length: " + list.length + "\n");
