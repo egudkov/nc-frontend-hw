d2cf108dc790de4a8f4f6ece63f876b3ce708822
Evgeny Gudkov <gudkoveyu@gmail.com>
2018-02-13 10:19:49 +0300

    Contains, toString were added. Shift returns removed elem. Test reverted.

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -4,17 +4,17 @@ class LinkedList {
     constructor() {
         this.head = {};
         this.head.value = null;
+        this.head.next = null;
         this.length = 0;
 
-        let iter = this.head;
-        iter.next = null;
+        let elem = this.head;
 
         for (let i = 0; i < arguments.length; i++) {
-            iter.next = {
+            elem.next = {
                 value: arguments[i],
                 next: null
             };
-            iter = iter.next;
+            elem = elem.next;
         }
 
         this.length = arguments.length;
@@ -29,74 +29,123 @@ class LinkedList {
     }
 
     get(index) {
-        let iter = this.head;
+        let elem = this.head;
 
         for (let i = 0; i < index + 1; i++) {
-            iter = iter.next;
+            elem = elem.next;
         }
-        return iter.value;
+        return elem.value;
     }
 
     set(index, value) {
-        let iter = this.head;
+        let elem = this.head;
 
         for (let i = 0; i < index + 1; i++) {
-            iter = iter.next;
+            elem = elem.next;
         }
-        iter.value = value;
+        elem.value = value;
     }
 
+    // Adds element to the list tail
     push() {
-        let iter = this.head;
+        let elem = this.head;
 
         for (let i = 0; i < this.length; i++) {
-            iter = iter.next;
+            elem = elem.next;
         }
 
         for (let i = 0; i < arguments.length; i++) {
-            iter.next = {
+            elem.next = {
                 value: arguments[i],
                 next: null
             };
-            iter = iter.next;
+            elem = elem.next;
             this.length++;
         }
     }
 
+    // Removes one element from the end of the list and returns it
     pop() {
-        let iter = this.head;
-
+        let elem = this.head;
 
         for (let i = 0; i < this.length - 1; i++) {
-            iter = iter.next;
+            elem = elem.next;
         }
 
-        let tmpValue = iter.next.value;
-        iter.next = null;
+        let tmpValue = elem.next.value;
+        elem.next = null;
         this.length--;
 
         return tmpValue;
     }
 
+    // Adds one or more elements at the beginning if the list
     unshift() {
-        let iter = this.head;
+        let elem = this.head;
         let tmpTail = this.head.next;
 
         for (let i = 0; i < arguments.length; i++) {
-            iter.next = {
+            elem.next = {
                 value: arguments[i],
                 next: null
             };
-            iter = iter.next;
+            elem = elem.next;
             this.length++;
         }
 
-        iter.next = tmpTail;
+        elem.next = tmpTail;
     }
 
+    // Removes one element from the beginning of the list and returns it
     shift() {
+        const firstElem = this.head.next;
         this.head.next = this.head.next.next;
         this.length--;
+
+        return firstElem.value;
+    }
+
+    // Checks if an element exists in the list
+    contains(value) {
+        let elem = this.head;
+        let isFound = false;
+
+        for (let i = 0; i < this.length; i++) {
+            elem = elem.next;
+            if (elem.value === value) {
+                isFound = true;
+                break;
+            }
+        }
+
+        return isFound;
+    }
+
+    toString() {
+        let elem = this.head;
+        let output = '[';
+
+        for (let i = 0; i < this.length; i++) {
+            elem = elem.next;
+            switch (typeof elem.value) {
+                case 'object':
+                    // TODO: what if object is not empty?
+                    output += '{}';
+                    break;
+                case 'string':
+                    output += `"${elem.value}"`;
+                    break;
+                default:
+                    output += elem.value;
+            }
+
+            if (i !== this.length - 1) {
+                output += ', ';
+            }
+        }
+        output += ']';
+
+        return output;
     }
 }
 
